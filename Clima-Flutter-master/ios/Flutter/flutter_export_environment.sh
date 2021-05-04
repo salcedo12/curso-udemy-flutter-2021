048a09690ab1b210189914c98240e06e1d102af1
Eduardo Salcedo <saluis7@misena.edu.co>
2021-05-04 08:24:43 -0500

    se agregan dos proyectos donde use una API una del clima y otra del bitcoin

--- Clima-Flutter-master/ios/Flutter/flutter_export_environment.sh
+++ Clima-Flutter-master/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\luis.salcedo\Desktop\curso-udemy-flutter\Clima-Flutter-master"
+export "FLUTTER_APPLICATION_PATH=C:\Users\luis.salcedo\Desktop\curso-udemy-flutter\curso-udemy-flutter-2021\Clima-Flutter-master"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
---
+++ bitcoin-flutter/lib/coin_data.dart
@@ -0,0 +1,61 @@
+//2. Import the required packages.
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+const List<String> currenciesList = [
+  'AUD',
+  'BRL',
+  'CAD',
+  'CNY',
+  'EUR',
+  'GBP',
+  'HKD',
+  'IDR',
+  'ILS',
+  'INR',
+  'JPY',
+  'MXN',
+  'NOK',
+  'NZD',
+  'PLN',
+  'RON',
+  'RUB',
+  'SEK',
+  'SGD',
+  'USD',
+  'ZAR'
+];
+
+const List<String> cryptoList = [
+  'BTC',
+  'ETH',
+  'LTC',
+];
+
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+const apiKey = 'D590149F-7542-43E1-9025-78B95D15DBF0';
+
+class CoinData {
+  //3. Create the Asynchronous method getCoinData() that returns a Future (the price data).
+  Future getCoinData() async {
+    //4. Create a url combining the coinAPIURL with the currencies we're interested, BTC to USD.
+    String requestURL = '$coinAPIURL/BTC/USD?apikey=$apiKey';
+    //5. Make a GET request to the URL and wait for the response.
+    http.Response response = await http.get(requestURL);
+
+    //6. Check that the request was successful.
+    if (response.statusCode == 200) {
+      //7. Use the 'dart:convert' package to decode the JSON data that comes back from coinapi.io.
+      var decodedData = jsonDecode(response.body);
+      //8. Get the last price of bitcoin with the key 'last'.
+      var lastPrice = decodedData['rate'];
+      //9. Output the lastPrice from the method.
+      return lastPrice;
+    } else {
+      //10. Handle any errors that occur during the request.
+      print(response.statusCode);
+      //Optional: throw an error if our request fails.
+      throw 'Problem with the get request';
+    }
+  }
+}
